34725cb59feceb3ab524e6e6327bf6551f506ab1
Linh Nguyen <redplane_dt@yahoo.com.vn>
2018-08-11 11:18:20 +0800

    - Initial commit.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PhpForum.Models;
+
+namespace PhpForum.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Topic()
+        {
+            return View();
+        }
+
+        public IActionResult TopicDetail()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        
+    }
+}
